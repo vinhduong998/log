4eefc5d4d3fc0550f1b3db9881e856eb8c1a4e79
ZetaMobile <zetamobile@Mac-mini-cua-ZetaMobile.local>
2022-02-16 11:11:54 +0700

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
 server.listen(process.env.PORT || 3000, function () {
-  console.log("Server's listening...");
+  console.log("Server's listening....");
 });
 
 let users = [];
