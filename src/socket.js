f59179412b9af1de2bea051de33d1b24dd206b56
vinhduong998 <vinhduong1200@gmail.com>
2024-01-11 14:32:44 +0700

    fix9

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import { logger } from './logger/loger';
 import { io } from 'socket.io-client';
 
 //Đổi link này
-const socket = io("https://log-91iajx7og-to-thu-has-projects.vercel.app/");
+const socket = io("http://localhost:3007");
 console.log('Connecting to server')
 
 let showJson = false;
