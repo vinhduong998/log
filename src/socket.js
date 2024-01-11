63f835b6710c008eb332932646fed76391cf144b
vinhduong998 <vinhduong1200@gmail.com>
2024-01-11 15:00:02 +0700

    fix13

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import { logger } from './logger/loger';
 import { io } from 'socket.io-client';
 
 //Đổi link này
-const socket = io("https://log-ashy.vercel.app/");
+const socket = io();
 console.log('Connecting to server')
 
 let showJson = false;
